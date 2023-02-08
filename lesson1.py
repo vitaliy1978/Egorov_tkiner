b9fbab1bd65a834e76bd096aa22b69b6a9ac0230
Vitaliy <vitaliy1978@mail.ru>
2023-02-08 11:16:54 +0600

    08.02.2023 11:16

--- lesson1.py
+++ lesson1.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 
 win = tk.Tk()
+photo = tk.PhotoImage(file='tkinter_icon.png')
+win.iconphoto(False, photo)
+win.config(bg='#E4F2EA')
 win.title('Моё первое графическое приложение на Python')
 win.geometry("500x1000+10+10")   # размеры окна + положение от левого вернего угла
 win.minsize(300,400)    # минимальный размер окна
