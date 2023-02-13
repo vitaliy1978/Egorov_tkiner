e881c79c6547a15d9d95403b08bf88cb80869da2
Vitaliy <vitaliy1978@mail.ru>
2023-02-13 15:49:34 +0600

    13.02.2023 Calcilator 15:49

--- lesson6.py
+++ lesson6.py
@@ -1,24 +1,52 @@
 import tkinter as tk
+from tkinter import messagebox
 
 
 def add_digit(digit):
     value = calc.get()
-    if value[0] == '0':
+    if value[0] == '0' and len(value) == 1:
         value = value[1:]
+    calc['state'] = tk.NORMAL
     calc.delete(0, tk.END)  # чищаем экран
-    calc.insert(0, value+digit)  # вставляем на экран переменную value
+    calc.insert(0, value + digit)  # вставляем на экран переменную value
+    calc['state'] = tk.DISABLED
 
 
 def add_operation(operation):
     value = calc.get()
     if value[-1] in '-+/*':
         value = value[:-1]
-    calc.delete(0, tk.END)  # чищаем экран
+    elif '+' in value or '-' in value or '*' in value or '/' in value:
+        calculate()
+        value = calc.get()
+    calc['state'] = tk.NORMAL
+    calc.delete(0, tk.END)  # очищаем экран
     calc.insert(0, value + operation)  # вставляем на экран переменную value
+    calc['state'] = tk.DISABLED
+
 
 def clear():
+    calc['state'] = tk.NORMAL
+    calc.delete(0, tk.END)  # очищаем экран
+    calc.insert(0, 0)
+    calc['state'] = tk.DISABLED
+
+
+def calculate():
+    value = calc.get()
+    if value[-1] in '-+/*':
+        value = value + value[:-1]
+    calc['state'] = tk.NORMAL
     calc.delete(0, tk.END)  # чищаем экран
-    calc.insert(0, '0')
+    try:
+        calc.insert(0, eval(value))  # вставляем на экран переменную value
+    except (NameError, SyntaxError):
+        messagebox.showinfo('Внимание', 'Нужно вводить только цифры! Вы ввели другие символы')
+        calc.insert(0,0)
+    except ZeroDivisionError:
+        messagebox.showinfo('Внимание', 'На ноль делить нельзя')
+        calc.insert(0, 0)
+    calc['state'] = tk.DISABLED
 
 def make_digit_button(digit):
     return tk.Button(text=digit, bd=3, font=('Arial', 14), command=lambda: add_digit(digit))
@@ -29,19 +57,33 @@ def make_operation_button(operation):
 
 
 def make_calc_button(operation):
-    return tk.Button(text=operation, bd=3, font=('Arial', 14), fg='red', command=lambda: add_operation(operation))
+    return tk.Button(text=operation, bd=3, font=('Arial', 14), fg='red', command=calculate)
+
 
 def make_clear_button():
-    return tk.Button(text='C', bd=3, font=('Arial', 12), fg='red', command=lambda: clear())
+    return tk.Button(text='C', bd=3, font=('Arial', 13), fg='red', command=clear)
 
+def press_key(event):
+    print(event.char)
+    if event.char.isdigit():
+        add_digit(event.char)
+    if event.char in '-+*/':
+        add_operation(event.char)
+    if event.char == '\r' or event.char == '=':
+        calculate()
 
 win = tk.Tk()
 win.geometry(f"240x282+100+200")
+win.maxsize(240, 282)
+win.minsize(240, 282)
 win['bg'] = '#D6EBEF'
 win.title('Калькулятор')
 
+win.bind('<Key>', press_key)
+
 calc = tk.Entry(win, justify=tk.RIGHT, font=('Arial', 16), width=15, bd=3)
-calc.insert(0,'0')
+calc.insert(0, '0')
+calc['state'] = tk.DISABLED
 calc.grid(row=0, column=0, columnspan=4, sticky='we', padx=5, pady=5)
 
 make_digit_button('1').grid(row=1, column=0, sticky='wens', padx=5, pady=5)
